854b182824b0d2769397b0ce7e178c3b81b26d10
Codynw42 <79997729+Codynw42@users.noreply.github.com>
2021-03-12 21:25:55 -0500

    Update workfile.py
    got the base structure written

--- workfile.py
+++ workfile.py
@@ -1,12 +1,39 @@
 import pygame
 import os  
 
+
+FPS = 60
 WHITE = (255, 255, 255)
-WIDTH, HEIGHT = 960, 540
-
-
+WIDTH, HEIGHT = 1400, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def draw_window():
-    WIN.fill(WHITE)
-    WIN.blit(WHITE)
+
+
+def draw_surface():
+    WIN.fill((WHITE))
+    pygame.display.update()
+    
+
+
+
+
+def mainloop():                     
+    
+    clock = pygame.time.Clock()
+    run = True                                 #The FIRST thing you always do when youre making a pygame, is write out this main function. 
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():       #The second thing you do, is check if the player has quit the game to end the loop with this function.
+            if event.type == pygame.QUIT:
+                run = False           
+                pygame.quit()                                   #Without a main loop, python would not be able to draw the window, it would start and stop immediately. 
+
+        
+        draw_surface()
+        
+        
+
+
+if __name__ == "__main__":
+    mainloop()                                
+        
